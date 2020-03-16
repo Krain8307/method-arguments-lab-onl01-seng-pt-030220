bd60bfc17418e77a0bbc48e2c35634b4fc8c24c9
N. Rainbow <kishrenee8307@gmail.com>
2020-03-16 02:05:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,7 @@
 
 def introduction(name="Dan")
   puts "Hi, my name is #{name}."
-end 
-
-
-def introduction(name="Dan", language="Ember.js")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
 
 
