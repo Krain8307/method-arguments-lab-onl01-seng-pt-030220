d31cdf188d333d167916a9f2a3ae17c5888500c1
N. Rainbow <kishrenee8307@gmail.com>
2020-03-16 02:09:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,7 @@
 def introduction(name="Dan")
   puts "Hello, my name is #{name}."
 end
+
+def introduction(name,language)
+  puts "Hello, my name is #{name} and I am learning to program with #{language}."
+end
